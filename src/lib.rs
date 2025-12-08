c75bfb3f76739758894fcf1bc7a7a1987c60ca86
Tranduy1dol <tmd.id2004@gmail.com>
2025-12-08 21:31:55 +0700

    feat: expand module exports for enhanced usability in big_int, field, and poly

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,8 @@ pub mod field;
 pub mod poly;
 pub mod traits;
 
-pub use crate::big_int::backend::*;
-pub use crate::big_int::u1024::U1024;
-pub use crate::field::element::FieldElement;
-pub use crate::poly::dense::DensePolynomial;
+pub use crate::big_int::{backend::*, u1024::U1024};
+pub use crate::field::{constants::*, element::FieldElement, montgomery::MontgomeryParams};
+pub use crate::poly::{dense::DensePolynomial, ntt::*};
+
 pub use traits::*;
